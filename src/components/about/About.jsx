f731c356fb235040649a9b2eea33769eefa59e2d
ChayanKM <mondalchayan0@gmail.com>
2023-05-23 14:30:37 +0530

    5th commit
    update resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/about.jpg"
-import CV from "../../assets/ChayanKM-CV.pdf"
+import CV from "../../assets/ChayanKr.Mondal_Resume.pdf"
 import Info from './Info'
 import LA from "../../assets/arrow-back-ios.png"
 import RA from "../../assets/arrow-forward-ios.png"
